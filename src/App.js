7c27e0999a2cfefe30f0d120aeeb758bcc9f9a27
Kolade-Junaid <koladejun@yahoo.com>
2020-01-21 12:43:48 +0000

    Modified the index.js and App.js files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-//TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React,{use, useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
+//TODO: STEP 1 - Import the useState hook.
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const [homeScore,sethomeScore] = useState(0);
+  const[awayScore,setawayScore] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+{/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button  onClick={() =>HomeTouchDownScore(homeScore +7)}
+
+           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={()=> HomeFieldScore(homeScore +3)} 
+          className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() =>AwayTouchDownScore(awayScore +7)}
+          className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={()=> AwayFieldScore(awayScore +3)} 
+           className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*import React from "react";*/
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import BottomRow from "./BottomRow";
+import Home from "./home";
+import Away from "./away";
+import HomeButtons from "./homeButtons";
+import AwayButtons from "./awayButtons";
+import Timer from "./timer";
+
+function App() {
+  const [lionsScore, lionsSetScore] = useState(0);
+  const [tigersScore, tigersSetScore] = useState(0);
+  const [quarter, setQuarter] = useState(0);
+  const [down, setDown] = useState(1);
+  const [timer, setTimer] = useState({ minutes: 10, seconds: 0 });
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      if (timer.seconds > 0) {
+        setTimer(({ seconds }) => ({
+          seconds: seconds - 1,
+          minutes: timer.minutes
+        }));
+      }
+      if (timer.seconds === 0) {
+        if (timer.minutes === 0) {
+          clearInterval(time);
+        } else {
+          setTimer(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59
+          }));
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(time);
+  });
+
+  const lionTd = () => {
+    lionsSetScore(lionsScore + 6);
+  };
+  const lionFg = () => {
+    lionsSetScore(lionsScore + 3);
+  };
+
+  const tigerTd = () => {
+    tigersSetScore(tigersScore + 6);
+  };
+  const tigerFg = () => {
+    tigersSetScore(tigersScore + 3);
+  };
+
+  const quarterChange = () => {
+    quarter < 4 ? setQuarter(quarter + 1) : setQuarter(0);
+  };
+  const changeDown = () => {
+    down < 4 ? setDown(down + 1) : setDown(1);
+  };
+
+  return (
+    <div className="container">
+      <section className="scoreboard">
+        <div className="topRow">
+          <Home score={lionsScore} />
+          <HomeButtons td={lionTd} fg={lionFg} />
+          <Timer timer={timer} />
+          <AwayButtons td={tigerTd} fg={tigerFg} />
+          <Away score={tigersScore} />
+        </div>
+        <BottomRow
+          quarter={quarter}
+          quarterChange={quarterChange}
+          down={down}
+          changeDown={changeDown}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
